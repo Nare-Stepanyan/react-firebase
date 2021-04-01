eebdee2ce2bfa46db504d10596f020ce58f79de3
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-04-01 22:52:48 +0300

    adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import "./firebase/config";
+import Header from "./Header";
+import React from "react";
 
 function App() {
-  return <div className="App">aaaaaaaa</div>;
+  return (
+    <div>
+      <Header></Header>
+      <div className="app">
+        <div className="ui grid container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
