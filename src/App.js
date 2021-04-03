72213e6328f8bc3ff4edfdc67c707348aa54af9a
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-04-03 13:04:22 +0300

    displaying user

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Header from "./Header";
 import React from "react";
 import Signup from "./pages/Signup";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./firebase/UserProvider";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header></Header>
-      <div className="app">
-        <div>
-          <Switch>
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
+    <UserProvider>
+      <BrowserRouter>
+        <Header></Header>
+        <div className="app">
+          <div>
+            <Switch>
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
