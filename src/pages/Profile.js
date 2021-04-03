0012f59f742dbd8faa94e65b253f0a5a5739efb2
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-04-03 23:19:51 +0300

    creating profile form

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useSession } from "../firebase/UserProvider";
+import { firestore } from "../firebase/config";
 
 const Profile = () => {
   const { user } = useSession();
-  if (!user) {
+  const params = useParams();
+  //const [register, setValue] = useForm();
+  const [userDocument, setUserDocument] = useState(null);
+
+  useEffect(() => {
+    const docRef = firestore.collection("users").doc(params.id);
+    const unsubscribe = docRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        const documentData = doc.data();
+        setUserDocument(documentData);
+      }
+    });
+    return unsubscribe;
+  }, [user.uid]);
+  if (!userDocument) {
     return null;
   }
   return (
     <div>
-      <p>Name : {user.displayName}</p>
-      <p>Email : {user.email}</p>
-      <p>ID : {user.uid}</p>
+      <p>{JSON.stringify(userDocument)}</p>
     </div>
   );
 };
