ef4ff69a896be13fb376879977450d417f709f5e
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-04-07 19:53:06 +0300

    image-upload

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { useSession } from "../firebase/UserProvider";
 import { firestore } from "../firebase/config";
 import { updateUserDocument } from "../firebase/user";
+import { ProfileImage } from "../ProfileImage";
 
 const Profile = () => {
   const { user } = useSession();
@@ -44,55 +45,60 @@ const Profile = () => {
 
   const formClassName = `${isLoading ? "loading" : ""}`;
   return (
-    <div className={styles.formWrapper}>
-      <form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.fields}>
-          <div className={styles.field}>
-            <label>Name</label>
-            <input type="text" name="name" {...register("name")} />
+    <div className={styles.profilePage}>
+      <div>
+        <ProfileImage id={params.id} />
+      </div>
+      <div className={styles.formWrapper}>
+        <form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.fields}>
+            <div className={styles.field}>
+              <label>Name</label>
+              <input type="text" name="name" {...register("name")} />
+            </div>
+            <div className={styles.field}>
+              <label>Email</label>
+              <input type="text" name="email" {...register("email")} />
+            </div>
+            <div className={styles.field}>
+              <label>Phone</label>
+              <input type="text" name="phone" {...register("phone")} />
+            </div>
           </div>
-          <div className={styles.field}>
-            <label>Email</label>
-            <input type="text" name="email" {...register("email")} />
+          <div className={styles.fields}>
+            <div className={styles.field}>
+              <label>Address</label>
+              <input type="text" name="address" {...register("address")} />
+            </div>
+            <div className={styles.field}>
+              <label>City</label>
+              <input type="text" name="city" {...register("city")} />
+            </div>
           </div>
-          <div className={styles.field}>
-            <label>Phone</label>
-            <input type="text" name="phone" {...register("phone")} />
+          <div className={styles.fields}>
+            <div className={styles.field}>
+              <label>State</label>
+              <input type="text" name="state" {...register("state")} />
+            </div>
+            <div className={styles.field}>
+              <label>Zip</label>
+              <input type="text" name="zip" {...register("zip")} />
+            </div>
           </div>
-        </div>
-        <div className={styles.fields}>
-          <div className={styles.field}>
-            <label>Address</label>
-            <input type="text" name="address" {...register("address")} />
+          <div className={styles.fields}>
+            <div className={styles.field}>
+              <label>Specialty</label>
+              <select name="specialty" {...register("specialty")}>
+                <option value="Frontend Developer">Frontend Developer</option>
+                <option value="Backend Developer">Backend Developer</option>
+                <option value="QA">QA</option>
+                <option value="Project Manager">Project Manager</option>
+              </select>
+            </div>
           </div>
-          <div className={styles.field}>
-            <label>City</label>
-            <input type="text" name="city" {...register("city")} />
-          </div>
-        </div>
-        <div className={styles.fields}>
-          <div className={styles.field}>
-            <label>State</label>
-            <input type="text" name="state" {...register("state")} />
-          </div>
-          <div className={styles.field}>
-            <label>Zip</label>
-            <input type="text" name="zip" {...register("zip")} />
-          </div>
-        </div>
-        <div className={styles.fields}>
-          <div className={styles.field}>
-            <label>Specialty</label>
-            <select name="specialty" {...register("specialty")}>
-              <option value="Frontend Developer">Frontend Developer</option>
-              <option value="Backend Developer">Backend Developer</option>
-              <option value="QA">QA</option>
-              <option value="Project Manager">Project Manager</option>
-            </select>
-          </div>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
