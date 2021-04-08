eb493bc8aca8480263a18b36810de695378d5112
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-04-08 19:13:00 +0300

    display users from admin panel

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "./../firebase/config";
+import styles from "./users.module.css";
 
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const usersRef = firestore.collection("users");
+    const unsubscribe = usersRef.onSnapshot((querySnapshot) => {
+      const users = querySnapshot.docs.map((doc) => doc.data());
+      setUsers(users);
+    });
+    return unsubscribe;
+  }, []);
+  return (
+    <div className={styles.userData}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Specialty</th>
+            <th>Address</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.uid}>
+              <td>
+                <Link to={`/profile/${user.uid}`}>{user.name}</Link>
+              </td>
+              <td>{user.specialty}</td>
+              <td>
+                {user.address} {user.city} {user.state} {user.zip}
+              </td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
